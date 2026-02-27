718c445b909dfaa74f4342e98ab9c8948aca0152
m.levashov <m.levashov@mediamagi.ru>
2026-02-27 15:19:44 +0300

    описание, тест с остановкой задачи

--- script.sh
+++ script.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# REQ_DATA это формат данных с тремя не известными:
-# \"urls\":[%s] - собираются из файла переменной CSV
-# \"login\":\"%s\",\"pass\":\"%s\" - логин и пароль для подключения к ftp, передаются как два первых параметра в скрипте при запуске
 
 # params
 # ссылка на сервис + версия
 URL="http://91.220.62.199:8080/v1"
+# REQ_DATA это формат данных с тремя неизвестными:
+# \"urls\":[%s] - собираются из файла переменной CSV
+# \"login\":\"%s\",\"pass\":\"%s\" - логин и пароль для подключения к ftp, передаются как два первых параметра в скрипте при запуске
 REQ_DATA='{\"in_dir\":\"C:\\\\Users\\\\Administrator\\\\Downloads\\\\InDir\",\"urls\":[%s],\"cmd\":\"ffmpeg.exe\",\"args\":[\"-i\",\"{input}\",\"-c:v\",\"copy\",\"-c:a\",\"aac\",\"-b:a\",\"64k\",\"{output}\"],\"out_ext\":\"mp4\",\"ftp\":{\"addr\":\"storage007.noisypeak.com:21\",\"login\":\"%s\",\"pass\":\"%s\"}}'
+# файл в котором лежат url откуда качать видео
 CSV="CS100files.txt"
+# кол-во одновременных выполнение задач
 TASK_COUNT=1
+# кол-во файлов в задаче
 FILE_COUNT=2
 
 # controllers
@@ -30,8 +33,19 @@ function get_state_string() {
     esac
 }
 
+# Принимает:
+# длинну массива
+# массив
+# состояние отслеживания
 function req_post() {
-    local arr=("$@")
+    local array_len="$1"
+    shift
+
+    local arr=("${@:1:$array_len}")
+    shift "$((array_len))"
+
+    local stop_state="$1"
+
     local formatted_string=''
     local first=true
 
@@ -52,7 +66,7 @@ function req_post() {
 
     local url=$URL'/'$TASK
     echo $url
-    sleep 3
+    sleep 1
 
     local tmpfile=$(mktemp)
     # Run curl, writing the body to a temporary file and the status code to stdout
@@ -65,59 +79,78 @@ function req_post() {
 
     if [ $status_code -eq 201 ]; then
         trimmed=$(echo "$body" | tr -d '"')
-        req_get $trimmed
+        tracking $trimmed $stop_state
+        return $?
     else
-        echo "Не получилось создать задачу на обработку"
+        echo "Не получилось создать задачу на обработку $status_code"
     fi
 }
 
 function req_get() {
-    local url=$URL'/'$TASK'/'$1
+    local url=$1
+    local cur_state=$2
+
     local tmpfile2=$(mktemp)
-    local cur_state=0
+    # Run curl, writing the body to a temporary file and the status code to stdout
+    local status_code=$(curl -s -w "%{http_code}" -o "$tmpfile2" "$url")
+    local body=$(cat "$tmpfile2")
+    rm "$tmpfile2" # Clean up the temporary file
+    #echo "Status Code: $status_code"
+    if [ $status_code -eq 200 ]; then
+        #echo "Response Body: $body"
+        if [ ${#body} -eq 0 ]; then
+            echo "Нет данных по запросу: $1"
+            return -1
+        fi
+        state=$(echo $body | grep -oP '"state":.+?,' | grep -Po '\d+')
+        #echo "state $state"
+        if [ $state -ne $cur_state ]; then
+            echo -n "-= "; echo -n "$(get_state_string $cur_state)"; echo " $1 =-"
+            echo -n "ID:              "; echo $body | jq -r '.id'
+            echo -n "InDir:           "; echo $body | jq -r '.in_dir'
+            echo -n "OutDir:          "; echo $body | jq -r '.out_dir'
+            echo -n "URLS:            "; echo $body | jq -r '.urls | join(", ")'
+            echo -n "Cmd:             "; echo $body | jq -r '.cmd'
+            echo -n "Args:            "; echo $body | jq -r '.args | join(", ")'
+            echo -n "OutExt:          "; echo $body | jq -r '.out_ext'
+            echo -n "Files:           "; echo $body | jq -r '.files | join(", ")'
+            echo -n "State:           "; get_state_string $state
+            echo -n "Msg:             "; echo $body | jq -r '.msg'
+            echo -n "Ftp:             "; echo $body | jq -r '.ftp[0]'
+        fi
+
+        return $state
+
+    fi
+
+    return -1
+}
+
+function req_delete() {
+    local task_id=$1
+    local url=$URL'/'$TASK'/'$task_id
+
+    echo "Delete $url"
+    curl --location --request DELETE $url
+}
+
+function tracking() {
+    local url=$URL'/'$TASK'/'$1
+    local stop_state=$2
+    local state=0
     echo "Отслеживание $url"; echo ""
 
     while true; do
-        # Run curl, writing the body to a temporary file and the status code to stdout
-        local status_code=$(curl -s -w "%{http_code}" -o "$tmpfile2" "$url")
-        local body=$(cat "$tmpfile2")
-        rm "$tmpfile2" # Clean up the temporary file
-        #echo "Status Code: $status_code"
-
-        if [ $status_code -eq 200 ]; then
-            #echo "Response Body: $body"
-            if [ ${#body} -eq 0 ]; then
-                echo "Нет данных по ключу $1"
-                return
-            fi
-
-            state=$(echo $body | grep -oP '"state":.+?,' | grep -Po '\d+')
-            #echo "state $state"
-            if [ $state -ne $cur_state ]; then
-                cur_state=$state
-                echo -n "-= "; echo -n "$(get_state_string $cur_state)"; echo " $1 =-"
-
-                echo -n "ID:              "; echo $body | jq -r '.id'
-                echo -n "InDir:           "; echo $body | jq -r '.in_dir'
-                echo -n "OutDir:          "; echo $body | jq -r '.out_dir'
-                echo -n "URLS:            "; echo $body | jq -r '.urls | join(", ")'
-                echo -n "Cmd:             "; echo $body | jq -r '.cmd'
-                echo -n "Args:            "; echo $body | jq -r '.args | join(", ")'
-                echo -n "OutExt:          "; echo $body | jq -r '.out_ext'
-                echo -n "Files:           "; echo $body | jq -r '.files | join(", ")'
-                echo -n "State:           "; get_state_string $cur_state
-                echo -n "Msg:             "; echo $body | jq -r '.msg'
-                echo -n "Ftp:             "; echo $body | jq -r '.ftp[0]'
-            fi
-            if [[ $state -eq 127 || $state -eq 5 ]]; then
-                echo "Отслеживание завершено $(get_state_string $state) $1"
-                return
-            fi
-            continue
+        req_get $url $state
+        state=$?
+        # остановка по статуса 127(ошибка), 5(Завершено)
+        # статус 255 - если нечего отслеживать
+        # можно задать до какого статуса ждем stop_state
+        if [[ $state -eq 127 || $state -eq 5 || $state -eq 255 || $state -eq $stop_state ]]; then
+            echo "Отслеживание завершено $(get_state_string $state) $1"
+            return $state
         fi
-
-        sleep 3
-        return
+        sleep 1
     done
 }
 
@@ -134,7 +167,7 @@ function read_file() {
             array+=($line)
             if [ $((idx % FILE_COUNT)) -eq 0 ]; then
                 #echo "$array"
-                req_post "${array[@]}" &
+                req_post "${#array[@]}" "${array[@]}" &
                 tasks=$((tasks+1))
                 array=()
             fi
@@ -151,7 +184,7 @@ function read_file() {
     fi
 }
 
-function main() {
+function test_download_exec_ftp() {
     echo "Нажмите Ctrl+C для выхода"
     while true; do
         read_file
@@ -160,15 +193,28 @@ function main() {
     done
 }
 
+# входящий параметр state
+# ждем этот состояние и отключаем задачу
+function test_stoping() {
+    local url=$1
+    local stop_state=$2
+    req_post 1 $url $stop_state
+}
+
 if [[ ${#LOGIN} -eq 0 || ${#PASS} -eq 0 ]]; then
     echo "Пустой параметр LOGIN $LOGIN PASS $PASS"
     return
 fi
 
-req_get "f5526c0c457337e5835672cac83c535c87ef192c2046f922e8e89f85f70a7627"
+#tracking "f5526c0c457337e5835672cac83c535c87ef192c2046f922e8e89f85f70a7627"
+#req_delete "f5526c0c457337e5835672cac83c535c87ef192c2046f922e8e89f85f70a7627"
 #my_array=("apple" "banana" "cherry")
 #req_post $my_array
 
-#main
+#test scripts
+test_stoping "https://storage007.noisypeak.com/mastervod/cs/CelebrityScene01032025.mp4" 1
+#test_stoping "https://storage007.noisypeak.com/mastervod/cs/CelebrityScene01032025.mp4" 2
+#test_stoping "https://storage007.noisypeak.com/mastervod/cs/CelebrityScene01032025.mp4" 3
+#test_download_exec_ftp
 
 echo "Все задачи завершены"
